ad341ed1e369934a46db8ee0ed5bc4e131b94667
Nishant S <nishant.s@IN-GN-52985.local>
2020-09-18 21:22:28 +0530

    New update

--- packer/user_data.sh
+++ packer/user_data.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 yum update -y
+cd /tmp
+sudo yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
+sudo systemctl enable amazon-ssm-agent
+sudo systemctl start amazon-ssm-agent
 sudo yum install epel-release -y
-sudo yum install ansible wget curl python3 -y
-sudo pip3 install PyMySQL
+sudo yum install ansible wget curl python3 python-pip -y
+sudo pip install PyMySQL
+curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
+unzip awscli-bundle.zip
+sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
+sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+sudo yum install -y http://rpms.remirepo.net/enterprise/remi-release-7.rpm
+sudo yum install -y yum-utils
+sudo yum-config-manager --enable remi-php73
