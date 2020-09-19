84a8e51971e11016b0c7731d5c13a1ceb49d3699
Nishant S <nishant.s@IN-GN-52985.local>
2020-09-19 23:11:48 +0530

    Added jenkins deployment

--- terraform/network/alb/alb.tf
+++ terraform/network/alb/alb.tf
@@ -8,7 +8,7 @@ module "mediawiki-tg" {
   target_type               = "instance"
   health_check_enabled      = "true"
   health_check_interval     = "30"
-  health_check_path         = "/"
+  health_check_path         = "/index.php/Main_Page"
   health_check_port         = "80"
   health_check_protocol     = "HTTP"
   health_check_timeout      = "300"
@@ -22,7 +22,7 @@ module "mediawiki-alb" {
   name                      = "mediawiki-alb"
   internal                  = "false"
   security_groups           = [data.aws_security_group.web.id]
-  subnets                   = ["subnet-0bc21093b6e6b937c", "subnet-ebb53490"]
+  subnets                   = ["subnet-0bc21093b6e6b937c", "subnet-ebb53490", "subnet-082ad0888493cb11b"]
   idle_timeout              = "60"
   enable_deletion_protection = "true"
   ip_address_type           = "ipv4"
@@ -36,30 +36,13 @@ module "mediawiki-alb_listener-http" {
   protocol                  = "HTTP"
   ssl_policy                = ""
   certificate_arn           = ""
-  default_action_type       = "redirect"
-  forward_target_group_arn  = ""
+  default_action_type       = "forward"
+  forward_target_group_arn  = "${module.mediawiki-tg.tg-arn}"
   redirect_host             = "#{host}"
   redirect_path             = "/#{path}"
   redirect_port             = "443"
   redirect_protocol         = "HTTPS"
   redirect_query            = "#{query}"
-  redirect_status_code      = "HTTP_301"
-}
-
-module "alb_listener-https" {
-  source                    = "../../modules/alb_listener"
-  load_balancer_arn         = "${module.mediawiki-alb.alb-arn}"
-  port                      = "443"
-  protocol                  = "HTTPS"
-  ssl_policy                = "TLS-1-2-2017-01"
-  certificate_arn           = ""
-  default_action_type       = "forward"
-  forward_target_group_arn  = "${module.mediawiki-tg.tg-arn}"
-  redirect_host             = ""
-  redirect_path             = ""
-  redirect_port             = ""
-  redirect_protocol         = "HTTPS"
-  redirect_query            = ""
   redirect_status_code      = "HTTP_301"
 }
 
